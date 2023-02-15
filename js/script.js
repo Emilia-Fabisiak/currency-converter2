3fcfa685cf607fd95fbd6d2face093ef021bf3ab
Emilia Fabisiak <chojnowska.emilia@gmail.com>
2023-02-15 20:55:37 +0100

    added init function

--- js/script.js
+++ js/script.js
@@ -1,9 +1,4 @@
 {
-    const formElement = document.querySelector(".js-form");
-    const amountElement = document.querySelector(".js-amount");
-    const currencyElement = document.querySelector(".js-currency");
-    const resultElement = document.querySelector(".js-result");
-
     const calculateResult = (amount, currency) => {
         const rateEUR = 4.71;
         const rateUSD = 4.35;
@@ -18,17 +13,27 @@
                 
             case "GBP":
                 return amount / rateGBP;
-        };
-    }
+        }
+    };
+    
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
 
     formElement.addEventListener("submit", (event) => {
         event.preventDefault();
+
+        const amountElement = document.querySelector(".js-amount");
+        const currencyElement = document.querySelector(".js-currency");
+        const resultElement = document.querySelector(".js-result");
 
         const currency = currencyElement.value;
         const amount = +amountElement.value;
 
         let result = calculateResult(amount, currency);
-        
+
         resultElement.value = `${amountElement.value} PLN = ${result.toFixed(2)} ${currency}`;
-    });
+        });
+    }; 
+    
+    init();
 }
