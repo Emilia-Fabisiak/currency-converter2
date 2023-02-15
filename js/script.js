da7fa0de16906696a572d2c23b63d1f965155584
Emilia Fabisiak <chojnowska.emilia@gmail.com>
2023-02-15 20:29:53 +0100

    changed let to const and reset last function

--- js/script.js
+++ js/script.js
@@ -1,33 +1,32 @@
-let formElement = document.querySelector(".js-form");
-let amountElement = document.querySelector(".js-amount");
-let currencyElement = document.querySelector(".js-currency");
-let resultElement = document.querySelector(".js-result");
+{
+    const formElement = document.querySelector(".js-form");
+    const amountElement = document.querySelector(".js-amount");
+    const currencyElement = document.querySelector(".js-currency");
+    const resultElement = document.querySelector(".js-result");
 
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
+    formElement.addEventListener("submit", (event) => {
+        event.preventDefault();
 
-    let rateEUR = 4.71;
-    let rateUSD = 4.35;
-    let rateGBP = 5.37;
+        const rateEUR = 4.71;
+        const rateUSD = 4.35;
+        const rateGBP = 5.37;
 
-    let currency = currencyElement.value;
-    let amount = +amountElement.value;
-    let result = resultElement.value;
+        const currency = currencyElement.value;
+        const amount = +amountElement.value;
+        let result = resultElement.value;
 
-    switch (currency) {
-        case "EUR":
-            result = amount / rateEUR;
-            break;
-        case "USD":
-            result = amount / rateUSD;
-            break;
-        case "GBP":
-            result = amount / rateGBP;
-            break;
-    };
+        switch (currency) {
+            case "EUR":
+                result = amount / rateEUR;
+                break;
+            case "USD":
+                result = amount / rateUSD;
+                break;
+            case "GBP":
+                result = amount / rateGBP;
+                break;
+        };
 
-resultElement.value = `${amountElement.value} PLN = ${result.toFixed(2)} ${currency}`;
-});
-
-formElement.addEventListener("reset", () => {
-})     
+        resultElement.value = `${amountElement.value} PLN = ${result.toFixed(2)} ${currency}`;
+    });
+}
